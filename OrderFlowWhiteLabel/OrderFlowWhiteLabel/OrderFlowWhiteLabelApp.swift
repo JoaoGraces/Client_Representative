fb047ffc5c1fc9154c39e70450de2df9d202c7c6
GabrielEduardoSilva <102189527+GabrielEduardoSilva@users.noreply.github.com>
2025-10-02 18:15:12 -0300

    [US21] chore: configurar estrutura inicial do projeto no Xcode

---
+++ OrderFlowWhiteLabel/OrderFlowWhiteLabel/OrderFlowWhiteLabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderFlowWhiteLabelApp.swift
+//  OrderFlowWhiteLabel
+//
+//  Created by Gabriel Eduardo on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct OrderFlowWhiteLabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
